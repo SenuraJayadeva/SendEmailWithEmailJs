91215436d3dc51c3d7dca4568c6a732fa4598d35
SenuraJayadeva <senurajayadeva@gmail.com>
2020-11-10 11:06:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import emailjs from "emailjs-com";
 
 function App() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_a0wzcfi",
+        e.target,
+        "USER_ID" //give you user id here
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div style={{ marginTop: "5%" }}>
+        <form onSubmit={sendEmail}>
+          <div class="form-group">
+            <label>Your Name</label>
+            <input
+              name="name"
+              type="text"
+              class="form-control"
+              placeholder="Enter Your Name"
+            />
+          </div>
+
+          <div class="form-group">
+            <label>Email address</label>
+            <input
+              name="email"
+              type="email"
+              class="form-control"
+              placeholder="Enter Your Email"
+            />
+          </div>
+
+          <div class="form-group">
+            <label>Message</label>
+
+            <textarea
+              name="message"
+              class="form-control"
+              placeholder="Enter Your Message"
+            ></textarea>
+          </div>
+
+          <button type="submit" class="btn btn-primary">
+            Send Email
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
